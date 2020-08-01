383b6c01dfdb113890605a79f64bc37e880a386e
Matt Moncayo <matthew.moncayo@gmail.com>
2020-08-01 00:33:16 -0700

    consolidated workout and exercise schemas into one route path

--- models/workout.js
+++ models/workout.js
@@ -1,23 +1,35 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
+
+const ExerciseSchema= new Schema({
+    type: String,
+    name: String,
+    distance: Number, 
+    duration: Number,
+    weight: Number,
+    sets: Number,
+    reps: Number
+});
 
 const WorkoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now()
     },
-    exercise: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise"
-        }
-    ],
+    exercises: [ExerciseSchema],
     totalDuration: {
         type: Number,
         default: 0
     }
 });
+
+WorkoutSchema.methods.setTotalDuration = function() {
+    let duration = 0;
+    this.exercises.forEach(exercise => {
+        duration += exercise.duration;
+    });
+    this.totalDuration= duration;
+}
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
