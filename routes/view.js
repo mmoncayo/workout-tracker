723d5ae146ab9f396f3470c064fe66f15c81cf2b
Matt Moncayo <matthew.moncayo@gmail.com>
2020-08-01 00:35:32 -0700

    updated view routing

--- routes/view.js
+++ routes/view.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const db = require("../models");
 const path = require("path");
 
 // index
@@ -12,9 +11,5 @@ router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-// stats
-router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/blah.html"));
-});
 
 module.exports = router;
